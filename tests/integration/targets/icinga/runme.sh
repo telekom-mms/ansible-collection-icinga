759c8745d001883ecb690dbd74b61271dc12f24f
Sebastian Gumprich <sebastian.gumprich@t-systems.com>
2023-07-31 10:45:26 +0200

    add integration tests

--- tests/integration/targets/icinga/runme.sh
+++ tests/integration/targets/icinga/runme.sh
@@ -2,7 +2,20 @@
 
 set -eux
 
-ansible-playbook icinga_agent.yml
-ansible-playbook icinga_agent.yml --check --diff
-ansible-playbook icinga_plugins.yml
-ansible-playbook icinga_plugins.yml --check --diff
+ansible-playbook icinga_agent.yml -e "@../../integration_config.yml" "$@"
+ansible-playbook icinga_agent.yml --check --diff -e "@../../integration_config.yml" "$@"
+ansible-playbook icinga_plugins.yml -e "@../../integration_config.yml" "$@"
+ansible-playbook icinga_plugins.yml --check --diff -e "@../../integration_config.yml" "$@"
+
+ansible-playbook icinga_plugins.yml -e "@../../integration_config.yml" "$@"
+ansible-playbook icinga_plugins.yml --check --diff -e "@../../integration_config.yml" "$@"
+
+ansible-playbook t_systems_mms.ansible_collection_icinga.domon_standard -e "@../../integration_config.yml" "$@"
+ansible-playbook t_systems_mms.ansible_collection_icinga.check_azure_oauth_token -e "@../../integration_config.yml" "$@"
+ansible-playbook t_systems_mms.ansible_collection_icinga.check_gitlab_scheduler -e "@../../integration_config.yml" "$@"
+ansible-playbook t_systems_mms.ansible_collection_icinga.check_https -e "@../../integration_config.yml" "$@"
+ansible-playbook t_systems_mms.ansible_collection_icinga.check_json -e "@../../integration_config.yml" "$@"
+ansible-playbook t_systems_mms.ansible_collection_icinga.check_json_azure_restapi -e "@../../integration_config.yml" "$@"
+ansible-playbook t_systems_mms.ansible_collection_icinga.check_json_azure_restapi_resourcehealth -e "@../../integration_config.yml" "$@"
+ansible-playbook t_systems_mms.ansible_collection_icinga.template_empty_host -e "@../../integration_config.yml" "$@"
+
